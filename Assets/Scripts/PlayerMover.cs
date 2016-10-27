2ad6946eb084ca21ab66a4b1e9c4c49758893fe3
Christopher Cabreros <chriscabreros27@gmail.com>
2016-10-27 16:46:02 -0700

    Some dev for basic functionality

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Boundary {
+    public float left;
+    public float right;
+    public float top;
+    public float bottom;
+}
+
 public class PlayerMover : MonoBehaviour {
 
+    public Boundary boundary;
     public float speed;
-    public float tilt;
+    public float tiltFront;
+    public float tiltSide;
 
     Rigidbody mRigidbody;
 
@@ -15,8 +25,17 @@ public class PlayerMover : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        /*
+        if ((Input.GetButton("Fire1") || Input.GetKeyDown("space")) &&
+        Time.time > mNextFire) {
+            GameObject clone = Instantiate(mShot, mShotSpawnTransform.position, mShotSpawnTransform.rotation) as GameObject;
+            mNextFire = Time.time + mFireRate;
+
+            //Play Sound
+            GetComponent<AudioSource>().Play();
+        }
+        */
+    }
 
     void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal") * speed;
@@ -25,12 +44,13 @@ public class PlayerMover : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         mRigidbody.velocity = movement;
 
-        /*
+        //Clamp position
         mRigidbody.position = new Vector3(
-            Mathf.Clamp(mRigidbody.position.x, mBoundary.xMin, mBoundary.xMax),
+            Mathf.Clamp(mRigidbody.position.x, boundary.left, boundary.right),
             0,
-            Mathf.Clamp(mRigidbody.position.z, mBoundary.zMin, mBoundary.zMax));
-            */
-        mRigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, mRigidbody.velocity.x * -tilt);
+            Mathf.Clamp(mRigidbody.position.z, boundary.bottom, boundary.top));
+
+        //Intentionally siwtch z and x
+        mRigidbody.rotation = Quaternion.Euler(mRigidbody.velocity.z * tiltSide, 0.0f, mRigidbody.velocity.x * -tiltFront);
     }
 }
---
+++ Assets/Scripts/StraightMover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StraightMover : MonoBehaviour {
+
+    public Vector3 initalVelocty;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        Vector3 normalizedVelocity = Vector3.Normalize(initalVelocty);
+
+        rb.velocity = normalizedVelocity * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
