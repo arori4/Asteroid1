2d3e1f3ac87b163858a610479e32c6d3949f9ba2
Christopher Cabreros <chriscabreros27@gmail.com>
2016-11-08 09:17:15 -0800

    More Backgrounds

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -3,16 +3,28 @@ using System.Collections;
 
 public class BackgroundScroller : MonoBehaviour {
 
-    public float speed;
-    public float length;
-    private Vector3 start;
+    public GameObject tile1;
+    public Vector3 tile1Start;
+    public GameObject tile2;
+    public Vector3 tile2Start;
+
+    public float tileSpeed = 0.3f;
+    public float tileLength = 25;
+    public Material[] backgrounds;
+
     
     void Start() {
-        start = transform.position;
+        //choose a random background
+        Material chosenBackground = backgrounds[Random.Range(0, backgrounds.Length)];
+
+        //set for each tile
+        tile1.GetComponent<Renderer>().material = chosenBackground;
+        tile2.GetComponent<Renderer>().material = chosenBackground;
     }
     
     void Update() {
-        float newPosition = Mathf.Repeat(Time.time * speed, length);
-        transform.position = start + Vector3.left * newPosition;
+        float newPosition = Mathf.Repeat(Time.time * tileSpeed, tileLength);
+        tile1.transform.position = tile1Start + Vector3.left * newPosition;
+        tile2.transform.position = tile2Start + Vector3.left * newPosition;
     }
 }
